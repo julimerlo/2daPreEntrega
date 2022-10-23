182998f77e61c140fecb208ce47895874b9bae59
Julian Merlo <merlo_julian@hotmail.com>
2022-10-23 16:49:04 -0300

    Trabajo final

--- js/apps.js
+++ js/apps.js
@@ -1,29 +1,3 @@
-/* const mostrarProductos = (productos) => {
-  const servicios = document.getElementById("servicios");
-
-  cortes.forEach((item) => {
-    const div = document.createElement("div");
-     div.classList.add("d-flex");
-    div.innerHTML += `<div class="card text-bg-dark mb-3" style="width: 18rem;">
-                        <h4>${item.nombre}</h4>
-                        <p>Precio: $${item.precio}</p>
-                        <p>${item.descripcion}</p>
-                        <button class="btn btn-primary" id=boton${item.id}>Agregar</button>
-                      </div>`;
-
-    servicios.appendChild(div);
-
-    const boton = document.getElementById(`boton${item.id}`);
-
-    boton.addEventListener("click", () => {
-      carritoIndex(item.id);
-      console.log(item.id);
-    });
-  });
-};
-
-mostrarProductos(cortes); */
-
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
@@ -46,3 +20,45 @@ fetch("./data.json")
       });
     });
   });
+
+
+const Reservar = () => {
+  let carritoEnLs = JSON.parse(localStorage.getItem("listaServicios2"));
+  if (!carritoEnLs || !carritoEnLs.length) {
+    Swal.fire({
+      position: "top-end",
+      icon: "warning",
+      title: "Seleccione al menos un servicio",
+      showConfirmButton: false,
+      timer: 1800,
+    });
+    return;
+  }
+
+  const nombre = document.getElementById('nombre').value;
+  const celular = document.getElementById('celular').value;
+
+  if (!nombre || !celular) {
+    Swal.fire({
+      position: "top-end",
+      icon: "warning",
+      title: "Debe completar todos los datos",
+      showConfirmButton: false,
+      timer: 1800,
+    });
+    return;
+  }
+
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Reserva confirmada",
+    showConfirmButton: false,
+    timer: 1800,
+  });
+
+  localStorage.clear();
+  document.getElementById("carrito-contenedor").replaceChildren();
+  document.getElementById('totalContainer').style.display = 'none';
+  document.getElementById('totalCarrito').innerHTML = 0;
+};
--- js/carritoIndex.js
+++ js/carritoIndex.js
@@ -1,20 +1,35 @@
 let carritoEnLs = JSON.parse(localStorage.getItem("listaServicios2"));
 let carritoDeCompras = [];
 
-console.log(carritoEnLs);
-
-if (carritoEnLs) {
-  carritoDeCompras = carritoEnLs;
-  carritoDeCompras.forEach((elemento) => {
+const renderServicios = () => {
+  if (carritoEnLs) {
+    carritoDeCompras = carritoEnLs;
+    let div = "";
+    let total = 0;
     const contenedorCarrito = document.getElementById("carrito-contenedor");
-    let div = document.createElement("div");
-    div.classList.add("productoEnCarrito");
-    div.innerHTML = ` <p>${elemento.nombre}</p>
-    <p>Precio: ${elemento.precio}</p> 
-    <button id="eliminar${elemento.id}" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button>`;
-    contenedorCarrito.appendChild(div);
-  });
-}
+    carritoDeCompras.forEach((elemento) => {
+      total += Number(elemento.precio);
+      div += `
+        <div class="productoEnCarrito">
+          <p>${elemento.nombre}</p>
+          <p>Precio: ${elemento.precio}</p>
+          <button onClick="borrarServicio(${elemento.id})" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button>
+        </div>
+      `;
+    });
+    contenedorCarrito.innerHTML = div;
+
+    if (total > 0) {
+      document.getElementById("totalContainer").style.display = "block";
+      document.getElementById("totalCarrito").innerHTML = total;
+    } else {
+      document.getElementById("totalContainer").style.display = "none";
+      document.getElementById("totalCarrito").innerHTML = 0;
+    }
+  }
+};
+
+renderServicios();
 
 const carritoIndex = (productoId) => {
   const contenedorCarrito2 = document.getElementById("carrito-contenedor");
@@ -29,27 +44,29 @@ const carritoIndex = (productoId) => {
       timer: 1800,
     });
   } else {
-    const renderProductosCarrito = () => {
-      let producto = cortes.find((producto) => producto.id == productoId);
-      carritoDeCompras.push(producto);
-      let div = document.createElement("div");
-      div.classList.add("productoEnCarrito");
-      div.innerHTML = ` <p>${producto.nombre}</p>
-                        <p>Precio: ${producto.precio}</p> 
-                        <button id="eliminar(${producto.id})" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button>`;
-      contenedorCarrito2.appendChild(div);
+    let producto = cortes.find((producto) => producto.id == productoId);
+    carritoDeCompras.push(producto);
+    let div = document.createElement("div");
+    div.classList.add("productoEnCarrito");
+    div.innerHTML = ` <p>${producto.nombre}</p>
+                      <p>Precio: ${producto.precio}</p> 
+                      <button onClick="borrarServicio(${producto.id})" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button>`;
+    contenedorCarrito2.appendChild(div);
 
-      /* Para borrar en el carrito. No me funciona!!*/
-      let botonBorrar = document.getElementById(`eliminar(${producto.id})`);
-      botonBorrar.addEventListener("click", () => {
-        borrarServicio(producto.id);
-      });
+    localStorage.setItem("listaServicios2", JSON.stringify(carritoDeCompras));
 
-      const guardarStorage2 = (clave, valor) => {
-        localStorage.setItem(clave, valor);
-      };
-      guardarStorage2("listaServicios2", JSON.stringify(carritoDeCompras));
-    };
+    let total = document.getElementById("totalCarrito").innerHTML;
+    total = Number(total) + Number(producto.precio);
+    console.log({ total2: total });
+    document.getElementById("totalCarrito").innerHTML = total;
+
+    if (total > 0) {
+      document.getElementById("totalContainer").style.display = "block";
+      document.getElementById("totalCarrito").innerHTML = total;
+    } else {
+      document.getElementById("totalContainer").style.display = "none";
+      document.getElementById("totalCarrito").innerHTML = 0;
+    }
 
     Swal.fire({
       position: "top-end",
@@ -58,27 +75,14 @@ const carritoIndex = (productoId) => {
       showConfirmButton: false,
       timer: 1800,
     });
-
-    renderProductosCarrito();
   }
 };
 console.log(carritoDeCompras);
 
-/* Funcion para borrar en el carrito.. no me funciona!!*/
 const borrarServicio = (productoId) => {
   const item = carritoDeCompras.find((producto) => producto.id == productoId);
   const index = carritoDeCompras.indexOf(item);
   carritoDeCompras.splice(index, 1);
+  localStorage.setItem("listaServicios2", JSON.stringify(carritoDeCompras));
+  renderServicios();
 };
-
-/* Hora */
-/* const botonHora = document.getElementById(`botonHora`);
-botonHora.addEventListener("click", () => {
-  const horarios = document.getElementById("horarios");
-  const div = document.createElement("div");
-  div.innerHTML += `<div>
-                        <h4>''</h4>
-                    </div>`;
-
-  horarios.appendChild(div);
-}); */
